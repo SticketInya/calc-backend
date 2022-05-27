0d72c9cd19fde3eeaba926b694fa0c5385956b40
JPatrik <jokhel.roland@gmail.com>
2022-05-27 11:47:44 +0200

    feat(index.ts): add number saving endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import express, { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 import 'dotenv/config';
 
 const PORT = parseInt(process.env.PORT || '3000', 10);
 const app = express();
+const allNumbers = [];
 
 app.disable('x-powered-by');
+app.use(express.json());
 
 //Return Saved Number - GET
 
 //Save Posted Number - POST
+app.post('/save', (req: Request, res: Response) => {
+    const { impNumber } = req.body as { impNumber: number };
+    if (isNaN(impNumber)) {
+        return res.status(400).send({ error: 'not a number' });
+    }
+    const newNumberEntry = {
+        id: uuidv4(),
+        number: impNumber,
+    };
+    allNumbers.push(newNumberEntry);
+    return res.status(201).send(newNumberEntry);
+});
 
 app.use('*', (_req: Request, res: Response) => {
     return res.status(404).send({ error: 'not found' });
